75f028722e00055ad7e143df0dd2331791cde761
khadija-b-a <khadijabenayed1@gmail.com>
2023-12-20 14:47:17 +0100

    modification de server.py pour intégrer le code de mongoDB

--- src/server.py
+++ src/server.py
@@ -1,17 +1,55 @@
 from flask import Flask
 import os
-import json
+
+from tools import mongoDB_tools as DB_tools
+from main import run
+import pymongo
 
 DEFAULT_PORT = "8080"
-MESSAGE = "Hello, World from Astrolabe expeditions !"
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def root():
-  return json.dumps({
-    "message": MESSAGE
-  })
+@app.route("/process/<file_id>", methods=["POST"])
+def root(file_id):
+  myclient = pymongo.MongoClient("")
+  db = myclient["Astrolabe"]
+
+  #connexion à la collection files de mongoDB
+  files = db.files
+
+  #transformation du file_id de string a ObjectId
+  file_id_object = DB_tools.object_file(file_id)
+
+  #extraire la ligne de files qui correspondant au file_id
+  information = DB_tools.find_file(files, file_id_object)
+  print(information)
+
+  #mise à jour du status du fichier à en cours
+  resultat = DB_tools.update_status(files,file_id_object,"in_process")
+  print(resultat.modified_count)
+
+  #insertion de lecture et traitement du dataset
+  dataset = run()
+
+  #insertion des colonnes manquantes dans le dataset
+  dataset["sensor_id"]=information["sensor_id"]
+  dataset["id"] = [file_id for i in range(len(dataset))]
+    
+  #connexion à la collection records de mongoDB
+  records=db.records
+    
+  #Insertion des données dans la BDD mongoDB
+  records.insert_many(dataset)
+
+  #mise à jour du status du fichier à terminé
+  fin = DB_tools.update_status(files,file_id_object,"processed")
+  print(fin.modified_count)
+
+  #fermeture du lien avec la BDD
+  myclient.close()
+  
+
 
 if __name__ == "__main__":
   # Scaleway's system will inject a PORT environment variable on which your application should start the server.
