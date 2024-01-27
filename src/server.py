fcb45d5016f8d8380f6d98af8855e9c3891aaf4f
cballevre <celestin.ballevre@protonmail.com>
2024-01-27 15:57:51 +0100

    feat: Add SQS queue

--- src/server.py
+++ src/server.py
@@ -33,55 +33,8 @@ def resource_not_found(e):
 
 @app.route("/process/<file_id>", methods=["POST"])
 def root(file_id):
-  load_dotenv() # load environment variables from .env file
-
-  myclient = pymongo.MongoClient(os.environ['MONGO_URI'])
-  db = myclient[os.environ['MONGO_DATABASE']]
-
-  #connexion à la collection files de mongoDB
-  files = db.files
-
-  #transformation du file_id de string a ObjectId
-  file_id_object = DB_tools.object_file(file_id)
-
-  #extraire la ligne de files qui correspondant au file_id
-  information = DB_tools.find_file(files, file_id_object)
-
-  if information is None:
-    abort(404, description="Resource not found")
-
-  #mise à jour du status du fichier à en cours
-  resultat = DB_tools.update_status(files,file_id_object,"in_process")
-
-  #insertion de lecture et traitement du dataset
-  s3 = connect_to_s3()
-  Key= "sensors/" + str(information["sensor_id"]) + "/" + file_id + ".csv"
-  response = s3.get_object(Bucket=os.environ['SCW_BUCKET'], Key = Key)
-  content = response['Body'].read().decode('utf-8')
-  data = pd.read_csv(io.StringIO(content), delimiter=";")
-  dataset = run(data)
-
-  #insertion des colonnes manquantes dans le dataset
-  dataset["sensor_id"] = str(information["sensor_id"])
-  dataset["file_id"] = str(file_id)
-  dataset["created_at"] = datetime.datetime.now()
-  dataset["updated_at"] = datetime.datetime.now()
-
-  data_dict = dataset.to_dict("records")
-
-  #connexion à la collection records de mongoDB
-  records=db.records
-
-  #Insertion des données dans la BDD mongoDB
-  records.insert_many(data_dict)
-
-  #mise à jour du status du fichier à terminé
-  fin = DB_tools.update_status(files,file_id_object,"processed")
-
-  #fermeture du lien avec la BDD
-  myclient.close()
-
-  return {"message": "success"}, 201
+  result = process_file(content['file_id'])
+  return result
 
 
 if __name__ == "__main__":
